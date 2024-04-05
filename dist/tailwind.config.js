8a33161d5d258d152d2c7cd6266dd6db4c581ef4
hayato94087 <hayato94087@gmail.com>
2024-04-05 23:19:01 +0900

    feat: noEmitを有効化

--- dist/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    plugins: [],
-};
-export default config;
